1f2db78da68f1d7f5bde48adce4ceaa7d0ffef22
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-24 12:35:40 +0100

    add pride palette

---
+++ Pride.js
@@ -0,0 +1,32 @@
+Pride={
+    "Rainbow": {
+      "colors": ['#E40303', '#FF8C00', '#FFED00', '#008026', '#004DFF', '#750787'],
+      "order": [1, 2, 3, 4, 5, 6],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Transgender": {
+      "colors": ['#5BCEFA', '#F5A9B8', '#FFFFFF', '#F5A9B8', '#5BCEFA'],
+      "order": [1, 2, 3, 4, 5],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Bisexual": {
+      "colors": ['#D60270', '#9B4F96', '#0038A8'],
+      "order": [1, 2, 3],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Nonbinary": {
+      "colors": ['#FFF430', '#FFFFFF', '#9C59D1', '#000000'],
+      "order": [1, 2, 3, 4],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Asexual": {
+      "colors": ['#000000', '#A4A4A4', '#FFFFFF', '#810081'],
+      "order": [1, 2, 3, 4],
+      "type": "qual",
+      "colorblindFriendly": false
+    }
+}
