86d0f2e12bebe23dd22ddbe83e1371c1acd222d4
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-24 14:01:27 +0100

    add second dropdown

--- src/chart.js
+++ src/chart.js
@@ -1,12 +1,18 @@
-function palettePlot(paletteName) {
-  const colPalette = PrettyCols[paletteName].colors;
+const paletteFiles = {
+  PrettyCols,
+  Pride
+};
+
+function palettePlot(fileName, paletteName) {
+  const colPalette = paletteFiles[fileName][paletteName].colors;
+
   const width = 600;
   const padding = 10;
   const n = colPalette.length;
   const boxSize = width / n;
   const height = boxSize + padding * 2;
 
-  const labels = d3.range(n).map(i => String.fromCharCode(65 + i)); 
+  const labels = d3.range(n).map(i => String.fromCharCode(65 + i));
 
   const data = d3.range(n).map(i => ({
     x: labels[i],
@@ -14,12 +20,12 @@ function palettePlot(paletteName) {
     colour: colPalette[i]
   }));
 
-  d3.select("#plot").html('');
+  d3.select("#plot").html(''); // Clear previous chart
 
   const chartContainer = d3.select("#plot")
     .style('background-color', "#F0F5F5")
     .style('padding', padding + 'px')
-    .style('width', width + padding*2 + 'px');
+    .style('width', width + padding * 2 + 'px');
 
   const svg = chartContainer
     .append('svg')
@@ -27,7 +33,7 @@ function palettePlot(paletteName) {
     .attr('preserveAspectRatio', 'xMidYMid meet')
     .style('width', '100%')
     .style('height', 'auto')
-    .append('g'); 
+    .append('g');
 
   const x = d3.scaleBand()
     .range([0, width])
@@ -41,38 +47,57 @@ function palettePlot(paletteName) {
     .attr('x', d => x(d.x))
     .attr('y', padding)
     .attr('width', x.bandwidth())
-    .attr('height', x.bandwidth()) 
+    .attr('height', x.bandwidth())
     .style('fill', d => d.colour)
     .style('stroke', 'none');
-  
 }
 
+function populateDropdowns() {
+  const fileNames = Object.keys(paletteFiles);
 
-function populateDropdown() {
-  const paletteNames = Object.keys(PrettyCols);
-  console.log(paletteNames)
-
-  // Create the select element for the dropdown
-  const dropdown = d3.select('#dropdown-container')
+  // File selector
+  const fileDropdown = d3.select('#file-dropdown-container')
     .append('select')
-    .attr('id', 'palette-dropdown')
-    .on('change', function() {
-      const selected = d3.select(this).property('value');
-      palettePlot(selected);
+    .attr('id', 'file-dropdown')
+    .on('change', function () {
+      const selectedFile = d3.select(this).property('value');
+      updatePaletteDropdown(selectedFile);
     });
 
-  // Add options to the dropdown
-  dropdown.selectAll('option')
+  fileDropdown.selectAll('option')
+    .data(fileNames)
+    .enter()
+    .append('option')
+    .text(d => d)
+    .attr('value', d => d);
+
+  // Initial palette load
+  updatePaletteDropdown(fileNames[0]);
+}
+
+function updatePaletteDropdown(fileName) {
+  const paletteNames = Object.keys(paletteFiles[fileName]);
+
+  // Remove and re-add dropdown to ensure fresh handler
+  d3.select('#palette-dropdown').remove();
+
+  const paletteDropdown = d3.select('#palette-dropdown-container')
+    .append('select')
+    .attr('id', 'palette-dropdown')
+    .on('change', function () {
+      const selectedPalette = d3.select(this).property('value');
+      palettePlot(fileName, selectedPalette); // <-- uses correct fileName
+    });
+
+  paletteDropdown.selectAll('option')
     .data(paletteNames)
     .enter()
     .append('option')
     .text(d => d)
     .attr('value', d => d);
 
-  // Trigger
-  dropdown.dispatch('change');
+  paletteDropdown.dispatch('change');
 }
-
 window.onload = function () {
-  populateDropdown();
+  populateDropdowns();
 };
