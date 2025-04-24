3cf7005520e69464d0d0856973408fd7747b7f1a
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-24 14:36:54 +0100

    update pride palette

--- Pride.js
+++ Pride.js
@@ -11,8 +11,20 @@ Pride={
       "type": "qual",
       "colorblindFriendly": false
     },
+    "Lesbian": {
+      "colors": ['#D52D00', '#FF9A56', '#FFFFFF', '#D362A4', '#A30262'],
+      "order": [1, 2, 3, 4, 5],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
     "Bisexual": {
       "colors": ['#D60270', '#9B4F96', '#0038A8'],
+      "order": [1, 2, 3],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Pansexual": {
+      "colors": ['#FF218C', '#FFD800', '#21B1FF'],
       "order": [1, 2, 3],
       "type": "qual",
       "colorblindFriendly": false
@@ -26,6 +38,36 @@ Pride={
     "Asexual": {
       "colors": ['#000000', '#A4A4A4', '#FFFFFF', '#810081'],
       "order": [1, 2, 3, 4],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Genderqueer": {
+      "colors": ['#B57EDC', '#FFFFFF', '#4A8123'],
+      "order": [1, 2, 3, 4],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Genderfluid": {
+      "colors": ['#FF75A2', '#FFFFFF', '#BE18D6', '#000000', '#333EBF'],
+      "order": [1, 2, 3, 4, 5],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Intersex": {
+      "colors": ['#FFD800', '#7902AA'],
+      "order": [1, 2],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Agender": {
+      "colors": ['#000000', '#B9B9B9', '#FFFFFF', '#A7D379',  '#FFFFFF', '#B9B9B9', '#000000'],
+      "order": [1, 2, 3, 4, 5, 6, 7],
+      "type": "qual",
+      "colorblindFriendly": false
+    },
+    "Aromantic": {
+      "colors": ['#3DA542', '#A7D379', '#FFFFFF', '#A9A9A9', '#000000'],
+      "order": [1, 2, 3, 4, 5],
       "type": "qual",
       "colorblindFriendly": false
     }
